8c02471c51940efcccc5590c0b0060ec27a20cf8
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-15 00:57:00 -0400

    Novos exercícios feitos. Novos tópicos

---
+++ src/entradaDeDados/entradaDeDadosInt.java
@@ -0,0 +1,20 @@
+package entradaDeDados;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class entradaDeDadosInt {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // Para ler numeros inteiros
+        int y;
+        y = sc.nextInt(); // Aqui armazena números com decimais
+        System.out.println("Você digitou: " + y);
+
+
+        sc.close();
+    }
+}
--- src/entradaDeDados/entradaDeDadosString.java
+++ src/entradaDeDados/entradaDeDadosString.java
@@ -1,10 +1,11 @@
 package entradaDeDados;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class entradaDeDados {
+public class entradaDeDadosString {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         
         String x; // Aqui armazena somente texto, simples, mas importante lembrar
@@ -14,10 +15,7 @@ public class entradaDeDados {
         
     
         
-        // Para ler numeros inteiros
-        int y;
-        y = sc.nextInt(); // Aqui armazena números com decimais
-        System.out.println("Você digitou: " + y);
+        
 
 
         // Para ler ponto flutuante
