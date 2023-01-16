1f91cf95f36fb77d2a0b2582a5492ad6d4d66788
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-16 11:47:49 -0400

    Novo tópico de estudo

---
+++ src/estruturaCondicional/estruturaCondicional.java
@@ -0,0 +1,7 @@
+package estruturaCondicional;
+
+public class estruturaCondicional {
+    public static void main(String[] args) {
+        // símbolos de expressões lógicas: && || !, respectivamente, "E" "OU" "NÃO"
+    }
+}
