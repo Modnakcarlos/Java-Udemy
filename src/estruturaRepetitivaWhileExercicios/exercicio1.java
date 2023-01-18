77ca3190418ad646f6f66485c2423299c75054c0
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-18 09:54:09 -0400

    Novos tópicos do curso

---
+++ src/estruturaRepetitivaWhileExercicios/exercicio1.java
@@ -0,0 +1,29 @@
+package estruturaRepetitivaWhileExercicios;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int senhaDoUsuario = sc.nextInt();
+        int senhaCorreta = 2002;
+
+        while (senhaDoUsuario != senhaCorreta) {
+            System.out.println("Senha Inválida");
+            senhaDoUsuario = sc.nextInt();    
+        }
+
+        System.out.println("Acesso Permitido");
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
---
+++ src/estruturaRepetitivaWhileExercicios/exercicio2.java
@@ -0,0 +1,21 @@
+package estruturaRepetitivaWhileExercicios;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
