26472dbc0b809a34fbe571d9cef11d2dad145ac6
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-18 10:15:52 -0400

    Exercicio sobre while (junto com if)

--- src/estruturaRepetitivaWhileExercicios/exercicio2.java
+++ src/estruturaRepetitivaWhileExercicios/exercicio2.java
@@ -7,8 +7,34 @@ public class exercicio2 {
 
         Scanner sc = new Scanner(System.in);
 
-        
+        int x, y;
 
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        while ( x != 0 && y != 0) {
+            
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro");
+            }
+            else if (x < 0 && y > 0) {
+                System.out.println("Segundo");
+            } 
+            else if (x < 0 && y < 0) {
+                System.out.println("Terceiro");
+            }
+            else {
+                System.out.println("Quarto");
+                
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+        
+        
+        }
+
+        System.out.println(" ");
 
 
 
