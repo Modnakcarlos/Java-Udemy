6b798608730ae050d8711aa76c9a2a88cd297d7e
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-18 11:15:38 -0400

    For na área

--- src/estruturaRepetitivaForExercicios/estruturaRepetitivaForExercicio.java
+++ src/estruturaRepetitivaForExercicios/estruturaRepetitivaForExercicio.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class estruturaRepetitivaForExercicio {
     public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int entradas = sc.nextInt();
-
         int soma = 0;
 
-        for(int i = 0; i <= entradas; i++) {
+        for(int i = 0; i < entradas; i++) {
             
             int valores = sc.nextInt();
             soma += valores;
 
         }
+
+
+    System.out.println(soma);
 
 
 
