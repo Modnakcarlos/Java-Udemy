2ce72dcc3b3203efa253bed4e8a8ec53f36bc8f5
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-24 16:35:45 -0400

    novo tópico

--- src/Funcoes/Funcoes.java
+++ src/Funcoes/Funcoes.java
@@ -1,9 +1,36 @@
 package Funcoes;
 
-
+import java.util.Scanner;
 
 public class Funcoes {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("adicione três números");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int maior = max(a, b, c);
+
+        mostrarResultado(maior);
+
+        sc.close();
+    }
+
+    public static int max(int a, int b, int c) {
+        int aux;
+        if (a > b && a > c) {
+            aux = a;
+        } else if (b > c) {
+            aux = b;    
+        } else {
+            aux = c;
+        }
+        return aux;
+    }
+
+    public static void mostrarResultado(int valor) {
+        System.out.println("maior número: " + valor);
     }
 }
