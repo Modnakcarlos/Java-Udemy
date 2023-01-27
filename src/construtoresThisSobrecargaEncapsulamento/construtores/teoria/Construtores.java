3c13195aa3114c2ee0e69d92b165dd707bd5fd42
Modnakcarlos <modnakcarlos1@gmail.com>
2023-01-27 12:24:25 -0400

    Construtores, sobrecarga e encapsulamento

---
+++ src/construtoresThisSobrecargaEncapsulamento/construtores/teoria/Construtores.java
@@ -0,0 +1,22 @@
+package construtoresThisSobrecargaEncapsulamento.construtores.teoria;
+
+public class Construtores {
+    /*
+        Construtores servem para deixar o código sem objetos vazios durante a execução.
+        Obriga o programador a informar os dados atribuídos as varáveis.
+
+        Por exemplo:
+
+            public String name;
+            public double price;
+            public int quantity;
+
+            public Product (String name, double price, int quantity) {
+                this.name = name;
+                this.price = price;
+                this.quantity = quantity;
+
+        Assim, quando o método Product for acionado, no mesmo momento as variáveis receberão suas atribiuições
+    
+    */
+}
---
+++ src/construtoresThisSobrecargaEncapsulamento/sobrecarga/Sobrecarga.java
@@ -0,0 +1,7 @@
+package construtoresThisSobrecargaEncapsulamento.sobrecarga;
+
+public class Sobrecarga {
+    /*
+        é tipo a criação de varios construtores com parâmetros diferentes!
+    */
+}
