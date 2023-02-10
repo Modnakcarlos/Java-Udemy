696ac1e6a71120f4c6e4498aaa21a112ae52aa92
Modnakcarlos <modnakcarlos1@gmail.com>
2023-02-10 11:11:24 -0400

    Enumerações

---
+++ src/enumeracoesComposicao/exemplo/application/Program.java
@@ -0,0 +1,7 @@
+package enumeracoesComposicao.exemplo.application;
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/enumeracoesComposicao/teoria/Teoria.java
@@ -0,0 +1,15 @@
+package enumeracoesComposicao.teoria;
+
+public class Teoria {
+    
+    /* É um tipo especial que serve para especificar de forma
+    literal um conjunto de constantes relacionadas.
+
+    Palavra-chave em Java: enum
+
+    Vantagem: melhor semântica, código mais legível e auxiliado pelo compilador
+    
+    */
+}
+
+
