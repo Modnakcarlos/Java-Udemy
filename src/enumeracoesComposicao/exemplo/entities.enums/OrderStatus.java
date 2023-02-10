6aa49cf96442514cd522a5ccaa42f5ef1b3eb8df
Modnakcarlos <modnakcarlos1@gmail.com>
2023-02-10 11:10:41 -0400

    Enumerações

---
+++ src/enumeracoesComposicao/exemplo/entities.enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package enumeracoesComposicao.exemplo.entities.enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
---
+++ src/enumeracoesComposicao/exemplo/entities/Order.java
@@ -0,0 +1,5 @@
+package enumeracoesComposicao.exemplo.entities;
+
+public class Order {
+    
+}
