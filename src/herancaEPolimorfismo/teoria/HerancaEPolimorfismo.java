f64bfc5da202cb0a57a7b52ade660e38dacbcbcc
Modnakcarlos <modnakcarlos1@gmail.com>
2023-03-06 09:19:08 -0400

    Herança e polimorfismo: tópicos novos

---
+++ src/herancaEPolimorfismo/teoria/HerancaEPolimorfismo.java
@@ -0,0 +1,18 @@
+package herancaEPolimorfismo.teoria;
+
+public class HerancaEPolimorfismo {
+    /*
+     * Herança é um tipo de associação que permite que uma classe herde "todos" os dados e comportamentos de outra
+     * 
+     * definições importantes:
+     *  VANTAGENS: 
+     *      reuso
+     *      polimorfismo
+     * 
+     * sintaxe: 
+     *  class a extends b {
+     * }
+     */
+
+
+}
